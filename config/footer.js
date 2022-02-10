e6a71897b8ad2547234571c2d073957cb65bc429
下羊 <xiayang.xy@alibaba-inc.com>
2022-02-10 16:01:21 +0800

    feat:footer

--- config/footer.js
+++ config/footer.js
@@ -2,41 +2,23 @@ module.exports = {
   style: 'dark',
   links: [
     {
-      title: '参考',
+      title: '快捷入口',
       items: [
         {
-          to: 'https://www.aliwork.com',
-          label: '宜搭官网',
+          to: 'https://alidocs.dingtalk.com/i/p/6gYG95Vo1D8nVGo9/docs/6gYG9qWrdjAO0Go9',
+          label: '帮助中心',
         },
         {
-          to: 'https://www.yuque.com/yida/support',
-          label: '宜搭使用手册',
-        },
-      ],
-    },
-    {
-      title: '社区',
-      items: [
-        {
-          label: '语雀文档讨论区',
-          href: 'https://www.yuque.com/yida/topics',
+          to: 'https://www.aliwork.com/price.html',
+          label: '版本',
         },
         {
-          label: '阿里云开发者论坛',
-          href: 'https://developer.aliyun.com/group/yida',
+          to: 'https://www.yuque.com/yida/topics',
+          label: '社区',
         },
         {
-          label: 'GitHub Issue',
-          href: 'https://github.com/alibaba/yida-developer/issues'
-        }
-      ],
-    },
-    {
-      title: '生态',
-      items: [
-        {
-          label: '钉钉开放平台',
-          href: 'https://open.dingtalk.com/document',
+          to: 'https://www.aliwork.com/developer/',
+          label: '开发者中心',
         },
       ],
     },
@@ -44,11 +26,32 @@ module.exports = {
       title: '更多',
       items: [
         {
-          label: '产品反馈',
-          href: 'https://www.aliwork.com/o/dev_feedback',
+          to: 'https://www.aliwork.com/workPlatform.html',
+          label: '关于宜搭',
+        },
+        {
+          to: 'https://www.aliwork.com/o/team',
+          label: '向团队推荐宜搭',
+        },
+        {
+          to: 'https://alidocs.dingtalk.com/i/p/6gYG95Vo1D8nVGo9',
+          label: '更新日志',
+        },
+      ],
+    },
+    {
+      title: '联系我们',
+      items: [
+        {
+          to: 'https://partner.dingtalk.com/opportunity_web.html?templateId=3fe61c97b34b4fa2b1582c52690c1471&channel=%E5%94%AE%E5%89%8D%E5%92%A8%E8%AF%A2-%E5%B0%8F%E8%9C%9C#/consultingService',
+          label: '售前咨询',
+        },
+        {
+          to: 'https://alidocs.dingtalk.com/i/p/6gYG95Vo1D8nVGo9/docs/6gYG9q9BkB4qDGo9',
+          label: '技术支持',
         },
       ],
     },
   ],
-  copyright: `Copyright © ${new Date().getFullYear()} 宜搭（Yida）. Built with Docusaurus.`,
+  copyright: `Copyright © ${new Date().getFullYear()} 钉钉（中国）信息技术有限公司和／或其关联公司 浙ICP备18037475号-4. Built with Docusaurus.`,
 }
