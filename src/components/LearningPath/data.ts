a372d22916d2387a3713252a42bbbb578ef8b0ff
jiushen <hanchao.shc@alibaba-inc.com>
2022-02-24 15:46:25 +0800

    feat: words

--- src/components/LearningPath/data.ts
+++ src/components/LearningPath/data.ts
@@ -1,292 +1,292 @@
 import {BankTwoTone, MessageTwoTone, CrownTwoTone, ProfileTwoTone} from '@ant-design/icons';
 export default [
   {
-    title: '了解',
-    icon: BankTwoTone,
+    title: '了解', 
+    icon: BankTwoTone, 
     children: [{
-      title: '产品简介',
+      title: '产品简介', 
       children: [
         {
-          title: '钉钉宜搭介绍',
-          path: 'https://www.yuque.com/yida/support/ytzzua',
-        },
+          title: '钉钉宜搭介绍', 
+          path: 'https://www.yuque.com/yida/support/ytzzua', 
+        }, 
         {
-          title: '开发者中心介绍',
-          path: '/docs/guide/about',
-        },
+          title: '开发者中心介绍', 
+          path: '/docs/guide/about', 
+        }, 
         {
-          title: '快速开始',
-          path: '/docs/guide/start',
-        },
+          title: '快速开始', 
+          path: '/docs/guide/start', 
+        }, 
         {
-          title: '名词解释',
-          path: '/docs/guide/keywords',
-        },
+          title: '名词解释', 
+          path: '/docs/guide/keywords', 
+        }, 
         {
-          title: '设计器功能介绍',
-          path: '/docs/guide/designer',
-        },
-      ],
+          title: '设计器功能介绍', 
+          path: '/docs/guide/designer', 
+        }, 
+      ], 
     }]
-  },
+  }, 
   {
-    title: '进阶',
-    icon: CrownTwoTone,
+    title: '进阶', 
+    icon: CrownTwoTone, 
     children: [
       {
-        title: '核心概念',
+        title: '核心概念', 
         children: [{
-          title: '全局变量',
+          title: '全局变量', 
           path: '/docs/guide/concept/state'
         }, {
-          title: '远程API',
+          title: '远程 API', 
           path: '/docs/guide/concept/dataSource'
         }, {
-          title: '页面生命周期',
+          title: '页面生命周期', 
           path: '/docs/guide/concept/lifecycle'
         }, {
-          title: '事件处理',
+          title: '事件处理', 
           path: '/docs/guide/concept/event'
         }, {
-          title: '条件渲染',
+          title: '条件渲染', 
           path: '/docs/guide/concept/condition'
         }, {
-          title: '循环渲染',
+          title: '循环渲染', 
           path: '/docs/guide/concept/loop'
         }, {
-          title: '自定义样式',
+          title: '自定义样式', 
           path: '/docs/guide/concept/style'
         }, {
-          title: '调试',
+          title: '调试', 
           path: '/docs/guide/concept/debug'
         }]
-      },
+      }, 
       {
-        title: 'API文档',
+        title: 'API 文档', 
         children: [{
-          title: '前端API',
+          title: '前端 API', 
           path: '/docs/guide/api'
         }, {
-          title: '远程开放API',
+          title: '远程开放 API', 
           path: '/docs/guide/openAPI'
         }]
-      },
+      }, 
       {
-        title: '高阶教程',
+        title: '高阶教程', 
         children: [{
-          title: 'TodoMVC',
-          path: '/docs/tutorial/todoMVC',
+          title: 'TodoMVC', 
+          path: '/docs/tutorial/todoMVC', 
         }]
-      },
-    ],
-  },
+      }, 
+    ], 
+  }, 
   {
-    title: '组件',
-    icon: ProfileTwoTone,
+    title: '组件', 
+    icon: ProfileTwoTone, 
     children: [
       {
-        title: '布局组件',
+        title: '布局组件', 
         children: [
           {
-            title: '选项卡',
-            path: '/docs/components/layout/tabsLayout',
-          },
-          {
-            title: '布局容器',
-            path: '/docs/components/layout/regionalContainer',
-          },
-        ],
-      },
-      {
-        title: '基础组件',
-        children: [
-          {
-            title: '图标',
-            path: '/docs/components/basic/icon',
-          },
-          {
-            title: '按钮',
-            path: '/docs/components/basic/button',
-          },
-          {
-            title: '文本',
-            path: '/docs/components/basic/text',
-          },
-          {
-            title: '图片',
-            path: '/docs/components/basic/image',
-          }, {
-            title: '链接',
-            path: '/docs/components/basic/link',
-          },
-          {
-            title: '链接块',
-            path: '/docs/components/basic/linkBlock',
-          },
-          {
-            title: '对话框',
-            path: '/docs/components/basic/dialog',
-          },
-          {
-            title: '抽屉',
-            path: '/docs/components/basic/drawer',
-          },
-          {
-            title: '视频播放',
-            path: '/docs/components/basic/video',
-          },
-        ],
-      },
-      {
-        title: '表单组件',
-        children: [
-          {
-            title: '数字输入框',
-            path: '/docs/components/form/number',
-          },
-          {
-            title: '单选',
-            path: '/docs/components/form/radio',
-          },
-          {
-            title: '下拉选择',
-            path: '/docs/components/form/select',
-          },
-          {
-            title: '多选',
-            path: '/docs/components/form/checkbox',
-          },
-          {
-            title: '下拉多选',
-            path: '/docs/components/form/selects',
-          },
-          {
-            title: '日期选择',
-            path: '/docs/components/form/dateSelect',
+            title: '选项卡', 
+            path: '/docs/components/layout/tabsLayout', 
           }, 
           {
-            title: '日期区间',
-            path: '/docs/components/form/daterange',
-          }, {
-            title: '上传图片',
-            path: '/docs/components/form/uploadImge',
-          },
-          {
-            title: '上传附件',
-            path: '/docs/components/form/uploadAppendix',
-          },
-          {
-            title: '输入框',
-            path: '/docs/components/form/textField',
-          },
-          {
-            title: '多行输入框',
-            path: '/docs/components/form/textInputs',
-          },
-          {
-            title: '人员搜索框',
-            path: '/docs/components/form/peopleSearch',
-          },
-          {
-            title: '评分',
-            path: '/docs/components/form/rateField',
-          },
-          {
-            title: '富文本编辑器',
-            path: '/docs/components/form/editorField',
-          },
-          {
-            title: '级联选择',
-            path: '/docs/components/form/cascadeSelectField',
-          },
-          {
-            title: '明细',
-            path: '/docs/components/form/tableField',
-          },
-        ],
-      },
+            title: '布局容器', 
+            path: '/docs/components/layout/regionalContainer', 
+          }, 
+        ], 
+      }, 
       {
-        title: '高阶组件',
+        title: '基础组件', 
         children: [
           {
-            title: 'JSX',
-            path: '/docs/components/advanced/JSX',
-          },
-          {
-            title: 'HTML',
-            path: '/docs/components/advanced/HTML',
-          },
-          {
-            title: 'Iframe',
-            path: '/docs/components/advanced/Iframe',
-          },
-          {
-            title: '底部通栏',
-            path: '/docs/components/advanced/bottomBanner',
-          },
-          {
-            title: '表格',
-            path: '/docs/components/advanced/table',
-          },
-          {
-            title: '轮播图',
-            path: '/docs/components/advanced/slider',
+            title: '图标', 
+            path: '/docs/components/basic/icon', 
           }, 
           {
-            title: '气泡提示',
-            path: '/docs/components/advanced/balloon',
+            title: '按钮', 
+            path: '/docs/components/basic/button', 
+          }, 
+          {
+            title: '文本', 
+            path: '/docs/components/basic/text', 
+          }, 
+          {
+            title: '图片', 
+            path: '/docs/components/basic/image', 
           }, {
-            title: '查询',
-            path: '/docs/components/advanced/filter',
-          },
+            title: '链接', 
+            path: '/docs/components/basic/link', 
+          }, 
           {
-            title: '翻页器',
-            path: '/docs/components/advanced/pagination',
-          },
+            title: '链接块', 
+            path: '/docs/components/basic/linkBlock', 
+          }, 
           {
-            title: '树形控件',
-            path: '/docs/components/advanced/tree',
-          },
+            title: '对话框', 
+            path: '/docs/components/basic/dialog', 
+          }, 
           {
-            title: '菜单',
-            path: '/docs/components/advanced/menu',
-          },
+            title: '抽屉', 
+            path: '/docs/components/basic/drawer', 
+          }, 
           {
-            title: '搜索',
-            path: '/docs/components/advanced/search',
-          },
+            title: '视频播放', 
+            path: '/docs/components/basic/video', 
+          }, 
+        ], 
+      }, 
+      {
+        title: '表单组件', 
+        children: [
           {
-            title: '进度条',
-            path: '/docs/components/advanced/progress',
-          },
+            title: '数字输入框', 
+            path: '/docs/components/form/number', 
+          }, 
           {
-            title: '时间轴',
-            path: '/docs/components/advanced/timeLine',
-          },
+            title: '单选', 
+            path: '/docs/components/form/radio', 
+          }, 
           {
-            title: '步骤条',
-            path: '/docs/components/advanced/steps',
-          },
-        ],
-      },
-    ],
-  },
+            title: '下拉选择', 
+            path: '/docs/components/form/select', 
+          }, 
+          {
+            title: '多选', 
+            path: '/docs/components/form/checkbox', 
+          }, 
+          {
+            title: '下拉多选', 
+            path: '/docs/components/form/selects', 
+          }, 
+          {
+            title: '日期选择', 
+            path: '/docs/components/form/dateSelect', 
+          }, 
+          {
+            title: '日期区间', 
+            path: '/docs/components/form/daterange', 
+          }, {
+            title: '上传图片', 
+            path: '/docs/components/form/uploadImge', 
+          }, 
+          {
+            title: '上传附件', 
+            path: '/docs/components/form/uploadAppendix', 
+          }, 
+          {
+            title: '输入框', 
+            path: '/docs/components/form/textField', 
+          }, 
+          {
+            title: '多行输入框', 
+            path: '/docs/components/form/textInputs', 
+          }, 
+          {
+            title: '人员搜索框', 
+            path: '/docs/components/form/peopleSearch', 
+          }, 
+          {
+            title: '评分', 
+            path: '/docs/components/form/rateField', 
+          }, 
+          {
+            title: '富文本编辑器', 
+            path: '/docs/components/form/editorField', 
+          }, 
+          {
+            title: '级联选择', 
+            path: '/docs/components/form/cascadeSelectField', 
+          }, 
+          {
+            title: '明细', 
+            path: '/docs/components/form/tableField', 
+          }, 
+        ], 
+      }, 
+      {
+        title: '高阶组件', 
+        children: [
+          {
+            title: 'JSX', 
+            path: '/docs/components/advanced/JSX', 
+          }, 
+          {
+            title: 'HTML', 
+            path: '/docs/components/advanced/HTML', 
+          }, 
+          {
+            title: 'Iframe', 
+            path: '/docs/components/advanced/Iframe', 
+          }, 
+          {
+            title: '底部通栏', 
+            path: '/docs/components/advanced/bottomBanner', 
+          }, 
+          {
+            title: '表格', 
+            path: '/docs/components/advanced/table', 
+          }, 
+          {
+            title: '轮播图', 
+            path: '/docs/components/advanced/slider', 
+          }, 
+          {
+            title: '气泡提示', 
+            path: '/docs/components/advanced/balloon', 
+          }, {
+            title: '查询', 
+            path: '/docs/components/advanced/filter', 
+          }, 
+          {
+            title: '翻页器', 
+            path: '/docs/components/advanced/pagination', 
+          }, 
+          {
+            title: '树形控件', 
+            path: '/docs/components/advanced/tree', 
+          }, 
+          {
+            title: '菜单', 
+            path: '/docs/components/advanced/menu', 
+          }, 
+          {
+            title: '搜索', 
+            path: '/docs/components/advanced/search', 
+          }, 
+          {
+            title: '进度条', 
+            path: '/docs/components/advanced/progress', 
+          }, 
+          {
+            title: '时间轴', 
+            path: '/docs/components/advanced/timeLine', 
+          }, 
+          {
+            title: '步骤条', 
+            path: '/docs/components/advanced/steps', 
+          }, 
+        ], 
+      }, 
+    ], 
+  }, 
   {
-    title: '常见问题',
-    icon: MessageTwoTone,
-    level: 1,
+    title: '常见问题', 
+    icon: MessageTwoTone, 
+    level: 1, 
     children: [{
-      title: '如何调用表单数据作为数据源？',
-      path: '/docs/guide/FAQ/q1',
-      width: '25%',
+      title: '将表单数据作为数据源', 
+      path: '/docs/guide/FAQ/q1', 
+      width: '25%', 
     }, {
-      title: '如何在自定义页面试用 ECharts？',
-      path: '/docs/guide/FAQ/q2',
-      width: '25%',
+      title: '加载 Apache ECharts', 
+      path: '/docs/guide/FAQ/q2', 
+      width: '25%', 
     }, {
-      title: '如何使用钉钉 JS API？',
-      path: '/docs/guide/FAQ/q3',
-      width: '25%',
+      title: '调用钉钉 JS API', 
+      path: '/docs/guide/FAQ/q3', 
+      width: '25%', 
     }]
   }
 ];
