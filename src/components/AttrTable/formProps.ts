6a04c59d416ed6ad02697e0cbcf0eb6a9a139735
下羊 <xiayang.xy@alibaba-inc.com>
2022-02-28 21:09:18 +0800

    feat:文档结构调整

---
+++ src/components/AttrTable/formProps.ts
@@ -0,0 +1,98 @@
+export default [
+  {
+    code: 'label',
+    type: 'string',
+    default: '-',
+    desc: '表单组件标题',
+  },
+  {
+    code: 'value',
+    type: 'number',
+    default: '-',
+    desc: '表单组件的默认值',
+  },
+  {
+    code: 'labelAlign',
+    type: `'left' | 'top'`,
+    default: `'top'`,
+    desc: '表单组件标题的位置',
+  },
+  {
+    code: 'labelColSpan',
+    type: 'number',
+    default: '4',
+    desc: '设置标题的占位宽度，配置值为栅格值，1代表1/24',
+  },
+  {
+    code: 'labelColOffset',
+    type: 'number',
+    default: '0',
+    desc: '设置标题的偏移位置，配置值为栅格值，1代表1/24',
+  },
+  {
+    code: 'wrapperColSpan',
+    type: 'number',
+    default: '0',
+    desc: '设置表单组件的占位宽度，设置值为栅格值，1代表1/24',
+  },
+  {
+    code: 'wrapperColOffset',
+    type: 'number',
+    default: '0',
+    desc: '设置表单组件的偏移位置，设置值为栅格值，1代表1/24',
+  },
+  {
+    code: 'labelTextAlign',
+    type: `'left' | 'right'`,
+    default: `'right'`,
+    desc: '表单组件的标题对齐方式',
+  },
+  {
+    code: 'tips',
+    type: 'String',
+    default: '-',
+    desc: '表单组件描述信息，展示在控件下方',
+  },
+  {
+    code: 'size',
+    type: '[Size](/docs/components/interface#size)',
+    default: `'medium'`,
+    desc: '表单组件尺寸',
+  },
+  {
+    code: 'behavior',
+    type: '[Behavior](/docs/components/interface#behavior)',
+    default: `'NORMAL'`,
+    desc: '表单组件显示状态',
+  },
+  {
+    code: 'labelTipsTypes',
+    type: '[LabelTipsTypes](/docs/components/interface#labelTipsTypes)',
+    default: `'none'`,
+    desc: '表单组件标题提示信息类型设置'
+  },
+  {
+    code: 'labelTipsIcon',
+    type: 'string',
+    default: '-',
+    desc: '表单组件标题提示信息入口图标，当labelTipsTypes取值为 **text** 或 **render** 时生效'
+  },
+  {
+    code: 'labelTipsText',
+    type: 'string',
+    default: '-',
+    desc: '表单组件标题提示文本信息，当labelTipsTypes取值为 **text** 时生效'
+  },
+  {
+    code: 'labelTipsRender',
+    type: '(props: INumberFieldProps) => ReactNode',
+    default: '-',
+    desc: '表单组件标题提示信息自定义渲染方法，当labelTipsTypes取值为 **render** 时生效'
+  },
+    {
+    code: 'validation',
+    type: '[Validation](/docs/components/interface#validation)',
+    default: '[]',
+    desc: '表单组件校验设置，具体使用详见[表单校验文档](/docs/guide/concept/validation)',
+  },
+];
--- src/components/AttrTable/index.tsx
+++ src/components/AttrTable/index.tsx
@@ -1,50 +1,56 @@
 import React from 'react';
-import {Table, TableProps} from 'antd';
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
+import { Table, TableProps } from 'antd';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import formProps from './formProps';
 import 'antd/lib/table/style/index.css';
 import './index.scss';
 
-const COLUMNS = [{
-  title: '属性',
-  dataIndex: 'code',
-  width: '20%',
-  render: (code) => <span style={{fontWeight: 600, display: 'inline-block'}}>{code}</span>
-}, {
-  title: '说明',
-  dataIndex: 'desc',
-  width: '30%',
-  render: (desc) => <ReactMarkdown children={desc}  remarkPlugins={[remarkGfm]}/>
-}, {
-  title: '类型',
-  dataIndex: 'type',
-  width: '10%',
-  render: (type) => <span style={{color: '#c41d7f', display: 'inline-block'}}>{type}</span>
-}, {
-  title: '默认值',
-  dataIndex: 'default',
-  width: '10%',
-  render: (val) => val === '-' ? <span>-</span> : <ReactMarkdown children={val}  remarkPlugins={[remarkGfm]}/>
-}, {
-  title: '数据示例',
-  dataIndex: 'demo',
-  width: '30%',
-  render: (demo) => <div style={{width: '100%'}}><ReactMarkdown children={demo} remarkPlugins={[remarkGfm]}/></div>
-}];
+const COLUMNS = [
+  {
+    title: '属性',
+    dataIndex: 'code',
+    width: '20%',
+    render: (code) => <span style={{ fontWeight: 600 }}>{code}</span>,
+  },
+  {
+    title: '说明',
+    dataIndex: 'desc',
+    width: '45%',
+    render: (desc) => <ReactMarkdown children={desc} remarkPlugins={[remarkGfm]} />,
+  },
+  {
+    title: '类型',
+    dataIndex: 'type',
+    className: 'attr-type',
+    width: '20%',
+    render: (type) => <ReactMarkdown children={type} remarkPlugins={[remarkGfm]} />,
+  },
+  {
+    title: '默认值',
+    dataIndex: 'default',
+    width: '15%',
+    render: (val) => (val === '-' ? <span>-</span> : <ReactMarkdown children={val} remarkPlugins={[remarkGfm]} />),
+  },
+];
 
 export interface TableRecord {
-  attr?: string;
   code?: string;
   type?: string;
-  demo?: string;
-  dynamic?: string;
   desc?: string;
+  default?: string;
 }
+export interface IAttrTableProps extends Pick<TableProps<TableRecord>, 'dataSource'> {
+  category?: string;
+}
+function AttrTable(props: IAttrTableProps) {
+  const { category, dataSource } = props;
 
-function AttrTable(props: Pick<TableProps<TableRecord>, 'dataSource'>) {
-  return (
-    <Table className="attr-table" dataSource={props.dataSource} pagination={false} columns={COLUMNS} />
-  );
+  const data =
+    category === 'form'
+      ? [...formProps, ...(dataSource || [])]
+      : [...(dataSource || [])].sort((a, b) => (a.code < b.code ? -1 : 1));
+  return <Table className="attr-table" dataSource={data} pagination={false} columns={COLUMNS} />;
 }
 
 export default AttrTable;
