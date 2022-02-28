5687d1dbf3ea581a94f47da845b3253977742cbd
下羊 <xiayang.xy@alibaba-inc.com>
2022-02-28 19:01:11 +0800

    feat:组件API展示调整

--- src/components/AttrTable/index.tsx
+++ src/components/AttrTable/index.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import {Table, TableProps} from 'antd';
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 import 'antd/lib/table/style/index.css';
 import './index.scss';
 
 const COLUMNS = [{
-  title: '属性名',
-  dataIndex: 'attr',
-  width: '20%',
-}, {
-  title: '属性Code',
+  title: '属性',
   dataIndex: 'code',
-  width: '10%',
+  width: '20%',
+  render: (code) => <span style={{fontWeight: 600, display: 'inline-block'}}>{code}</span>
+}, {
+  title: '说明',
+  dataIndex: 'desc',
+  width: '30%',
+  render: (desc) => <ReactMarkdown children={desc}  remarkPlugins={[remarkGfm]}/>
 }, {
   title: '类型',
   dataIndex: 'type',
   width: '10%',
+  render: (type) => <span style={{color: '#c41d7f', display: 'inline-block'}}>{type}</span>
+}, {
+  title: '默认值',
+  dataIndex: 'default',
+  width: '10%',
+  render: (val) => val === '-' ? <span>-</span> : <ReactMarkdown children={val}  remarkPlugins={[remarkGfm]}/>
 }, {
   title: '数据示例',
   dataIndex: 'demo',
   width: '30%',
-}, {
-  title: '备注',
-  dataIndex: 'desc',
-  width: '20%',
+  render: (demo) => <div style={{width: '100%'}}><ReactMarkdown children={demo} remarkPlugins={[remarkGfm]}/></div>
 }];
 
 export interface TableRecord {
@@ -36,7 +43,7 @@ export interface TableRecord {
 
 function AttrTable(props: Pick<TableProps<TableRecord>, 'dataSource'>) {
   return (
-    <Table className="attr-table" dataSource={props.dataSource} columns={COLUMNS} pagination={false} />
+    <Table className="attr-table" dataSource={props.dataSource} pagination={false} columns={COLUMNS} />
   );
 }
 
