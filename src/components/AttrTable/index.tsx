90f49871d82592b8a7dc869e97e3831ed4cdda6a
xiayang.xy <xiayang.xy@alibaba-inc.com>
2022-05-18 21:47:09 +0800

    fix: gihub站点组件属性类型跳转连接失效问题

--- src/components/AttrTable/index.tsx
+++ src/components/AttrTable/index.tsx
@@ -1,39 +1,11 @@
 import React from 'react';
 import { Table, TableProps } from 'antd';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import formProps from './formProps';
 import 'antd/lib/table/style/index.css';
 import './index.scss';
-
-const COLUMNS = [
-  {
-    title: '属性',
-    dataIndex: 'code',
-    width: '20%',
-    render: (code) => <div style={{ fontWeight: 600 }}>{code}</div>,
-  },
-  {
-    title: '说明',
-    dataIndex: 'desc',
-    width: '45%',
-    render: (desc) => <ReactMarkdown children={desc} remarkPlugins={[remarkGfm]} />,
-  },
-  {
-    title: '类型',
-    dataIndex: 'type',
-    className: 'attr-type',
-    width: '20%',
-    render: (type) => <ReactMarkdown children={type} remarkPlugins={[remarkGfm]} />,
-  },
-  {
-    title: '默认值',
-    dataIndex: 'default',
-    width: '15%',
-    wordBreak: true,
-    render: (val) => ((val === '-' || !val) ? <div> - </div> : <ReactMarkdown children={val} remarkPlugins={[remarkGfm]} />),
-  },
-];
 
 export interface TableRecord {
   code?: string;
@@ -46,6 +18,39 @@ export interface IAttrTableProps extends Pick<TableProps<TableRecord>, 'dataSour
 }
 function AttrTable(props: IAttrTableProps) {
   const { category, dataSource } = props;
+  const {siteConfig} = useDocusaurusContext();
+  const updateBaseUrl = (mk: string): string => {
+    return mk.replace(/\[(.*)\]\(\/docs\/(.*)\)/g, (m, text, path) => `[${text}](${siteConfig.baseUrl}docs/${path})`);
+  }
+
+  const COLUMNS = [
+    {
+      title: '属性',
+      dataIndex: 'code',
+      width: '20%',
+      render: (code) => <div style={{ fontWeight: 600 }}>{code}</div>,
+    },
+    {
+      title: '说明',
+      dataIndex: 'desc',
+      width: '45%',
+      render: (desc) => <ReactMarkdown children={updateBaseUrl(desc)} remarkPlugins={[remarkGfm]} />,
+    },
+    {
+      title: '类型',
+      dataIndex: 'type',
+      className: 'attr-type',
+      width: '20%',
+      render: (type) => <ReactMarkdown children={updateBaseUrl(type)} remarkPlugins={[remarkGfm]} />,
+    },
+    {
+      title: '默认值',
+      dataIndex: 'default',
+      width: '15%',
+      wordBreak: true,
+      render: (val) => ((val === '-' || !val) ? <div> - </div> : <ReactMarkdown children={updateBaseUrl(val)} remarkPlugins={[remarkGfm]} />),
+    },
+  ];
 
   const data =
     (category === 'form'
